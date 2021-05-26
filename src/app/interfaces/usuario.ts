2e3b5a95d00405092f15f4c6cca517b4b114d54d
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-05-26 18:21:08 -0300

    interface de usuario

---
+++ src/app/interfaces/usuario.ts
@@ -0,0 +1,23 @@
+export interface Usuario {
+    nome?: string;
+    telefone?: number;
+    cpf?: number;
+    nascimento?: Date;
+    sexo?: string;
+    email?: string;
+    senha?: string;
+    dadosEndereco?:{
+        cep?: number;
+        endereco?: string;
+        numero?: number;
+        complemento?: string;
+    };
+    produtosFavoritos?: string[];
+    carrinho?: [
+        {
+            id?: string;
+            quantidade?: number;
+            valorTotal?: number;
+        }
+    ];
+}
