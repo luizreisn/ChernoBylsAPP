16febe82266cbfeda68ff26e7a4c12c76eeaaee7
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-05-28 20:23:15 -0300

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { AlertController, IonSlides } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Avisos, BotoesMenu, BotoesMenuEsp } from '../interfaces/botoes-menu';
+import { Produto } from '../interfaces/produto';
 import { Usuario } from '../interfaces/usuario';
 import { AuthService } from '../services/auth.service';
+import { BotoesMenuService } from '../services/botoes-menu.service';
+import { ProdutoService } from '../services/produto.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +15,22 @@ import { AuthService } from '../services/auth.service';
 })
 export class HomePage {
 
-  public userData = this.authService.userData;
+  public usuario: Usuario = {};
+  public usuarioId: string = null;
+  public usuarioSubscription: Subscription;
+
+  public produtos = new Array<Produto>();
+  private produtosSubscription: Subscription;
+  public produtosFiltrados = this.produtos;
+
+  public botoesMenu = new Array<BotoesMenu>();
+  private botoesMenuSubscription: Subscription;
+
+  public botoesMenuEsp = new Array<BotoesMenuEsp>();
+  private botoesMenuEspSubscription: Subscription;
+
+  public avisos = new Array<Avisos>();
+  private avisosSubscription: Subscription;
 
   public focar = 0;
 
@@ -22,7 +42,46 @@ export class HomePage {
   }
 
   constructor(private authService: AuthService,
-              private alertCtrl: AlertController) {        
+              private produtoService: ProdutoService,
+              private botoesMenuService: BotoesMenuService,
+              private alertCtrl: AlertController) { 
+    this.produtosSubscription = produtoService.getProdutos().subscribe(data =>{
+      this.produtos = data;
+    });
+    this.botoesMenuSubscription = botoesMenuService.getBotoesMenu().subscribe(data => {
+      this.botoesMenu = data;
+    });
+    this.botoesMenuEspSubscription = botoesMenuService.getBotoesMenuEsp().subscribe(data => {
+      this.botoesMenuEsp = data;
+    });     
+    this.avisosSubscription = botoesMenuService.getAvisos().subscribe(data => {
+      this.avisos = data;
+    });
+    this.carregarDados();
+  }
+
+  public async carregarDados(){
+    this.usuarioId = (await this.authService.getAuth().currentUser).uid
+    this.usuarioSubscription = this.authService.getUsuario(this.usuarioId).subscribe(data => {
+      this.usuario = data;
+    })
+  }
+
+  ngOnDestroy(){
+    this.produtosSubscription.unsubscribe();
+    this.botoesMenuSubscription.unsubscribe();
+    this.botoesMenuEspSubscription.unsubscribe();
+    this.avisosSubscription.unsubscribe();
+  }
+
+  public busca(ev: CustomEvent){
+    let val= ev.detail.value;
+    if(val && val.trim() !== ''){
+      this.produtosFiltrados = this.produtos.filter(termo =>
+        termo.nome.toLocaleLowerCase().indexOf(val.toLocaleLowerCase().trim()) > -1)
+    }else{
+      this.produtosFiltrados = this.produtos;
+    }
   }
 
   public focado(){
