d7eda95c74bfdbbd629b675e202c1fb5c6094b60
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-05-28 20:21:57 -0300

    pagina de endereço

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,27 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'endereco',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./endereco/endereco.module').then( m => m.EnderecoPageModule)
+  },
+  {
+    path: 'perfil',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'menus',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./menus/menus.module').then( m => m.MenusPageModule)
+  },
+  {
+    path: 'menus/:id',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./menus/menus.module').then( m => m.MenusPageModule)
+  },
+  
 ];
 
 @NgModule({
